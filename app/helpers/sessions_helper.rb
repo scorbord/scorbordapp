357d853e7c20140778ab2b4d8c1cee6cac51a2a1
Steven Holt <steven@scorbord.com>
2016-03-27 15:01:17 -0400

    Finished User Account management

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,10 +39,25 @@ module SessionsHelper
     end
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	# Returns 'true' if a user is logged in and 'false' if not
 	def logged_in?
 		!current_user.nil?
 	end
+
+	# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+  	redirect_to(session[:forwarding_url] || default)
+  	session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 
 
 

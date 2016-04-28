17c54b84b028724e8491802cd6fd855a68c6eb3b
Steven Holt <steven@scorbord.com>
2016-04-27 21:26:28 -0400

    Fixed delete team process

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,6 @@
 class TeamsController < ApplicationController
+	before_action :logged_in_user
+	#before_action :correct_user?, only: :destroy
 
 	def new
 		@team = Team.new
@@ -28,11 +30,21 @@ class TeamsController < ApplicationController
 		end
 	end
 
+	def destroy
+		current_user.teams.find_by(id: params[:id]).destroy
+		flash[:success] = "Team successfully deleted"
+		redirect_to current_user
+	end
+
 	private
 
 		def team_params
 			params.require(:team).permit(:name, :sport)
 		end
 
+		def correct_user?
+			@team = current_user.teams.find(params[:id])
+      redirect_to current_user if @team.nil?
+    end
 
 end

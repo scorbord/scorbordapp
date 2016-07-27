bde08f0f8d3fce7cceaf577661daee70d8ebddef
Steven M. Holt <holtstevenmichael@gmail.com>
2016-07-27 10:23:36 -0400

    Add GitBranch Header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def branch_info
+    branch_name = `git rev-parse --abbrev-ref HEAD`
+		branch_name == "master\n" ? style = "master" : style = ""
+		content_tag :div, :class => "gitbranch #{style}" do
+			content_tag :span, branch_name, :class => ""
+		end
+	end
+
 end

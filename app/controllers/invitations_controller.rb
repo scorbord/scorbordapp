494632b0b81df22e3c7ce33b58e400723c1fb548
Steven M. Holt <holtstevenmichael@gmail.com>
2017-03-16 06:16:46 -0400

    Fix layout rendering to consider BETA_STATUS

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,13 +4,10 @@ class InvitationsController < ApplicationController
     redirect_to(login_path) unless Invitation.find_by(token: params[:invitation_id]).present?
   end
 
+  layout :beta_layout?
+
   def new
     @invitation = Invitation.new
-    if ENV['BETA_STATUS'] == "ON"
-      render :layout => 'beta'
-    else
-      render :layout => 'materialhome'
-    end
   end
 
   def create
@@ -22,24 +19,32 @@ class InvitationsController < ApplicationController
         redirect_to root_path
         flash[:success] = "Thank you, #{@invitation.first_name}.  Your request has been submitted."
       else
-        render 'new', :layout => 'materialhome'
+        render 'new'
       end
     else
       @invitation.errors.set(:email, ['has already been signed up!'])
-      render 'new', :layout => 'materialhome'
+      render 'new'
     end
   end
 
 	def redeem
 		@invitation ||= Invitation.find_by(token: params[:invitation_id])
 		@user = User.find_by(email: @invitation.email, first_name: @invitation.first_name, last_name: @invitation.last_name)
-    render :layout => 'beta'
 	end
 
   private
 
     def invitation_params
       params.require(:invitation).permit(:first_name, :last_name, :email, :token)
+    end
+
+    def beta_layout?
+      if ENV['BETA_STATUS'] == "ON"
+        'beta'
+      else
+        'materialhome'
+      end
+
     end
 
 end

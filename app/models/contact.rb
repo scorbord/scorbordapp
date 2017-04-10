00c208497b24f3ff41fcca96e17143f862684dfb
Steven M. Holt <holtstevenmichael@gmail.com>
2017-04-09 20:37:09 -0400

    Fix enum assignment

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,20 +2,20 @@ class Contact < ActiveRecord::Base
 	belongs_to :prospect
 
 	enum relation: {
-		Father: 1,
-		Mother: 2,
-		'Step Father': 3,
-		'Step Mother': 4,
-		Aunt: 5,
-		Uncle: 6,
-		Grandparent: 7,
-		Guardian: 8
+		'Father' => 1,
+		'Mother' => 2,
+		'Step Father' => 3,
+		'Step Mother' => 4,
+		'Aunt' => 5,
+		'Uncle' => 6,
+		'Grandparent' => 7,
+		'Guardian' => 8
 	}
 
 	enum salutation: {
-		'Mr.': 1,
-		'Mrs.': 2,
-		'Ms.': 3
+		'Mr.' => 1,
+		'Mrs.' => 2,
+		'Ms.' => 3
 	}
 
 end
